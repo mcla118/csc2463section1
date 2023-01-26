c0665d8a6c05c8161ee95b0cd1599fcbf7fd9a76
mcla118 <mcla118@lsu.edu>
2023-01-26 14:35:34 -0600

    Assignment1-ex1

---
+++ Assignment1-ex1/ex1.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(400, 200);
+  
+  }
+  
+  function draw() {
+    background(133, 225, 0);
+
+// Create a white circle
+circle (110, 100, 165);
+describe('white circle with black outline on left side of canvas');
+
+// Create a white square
+square (220, 20, 165);
+describe('white square with black outline on right side of canvas');
+
+
+  }
+
+ 
